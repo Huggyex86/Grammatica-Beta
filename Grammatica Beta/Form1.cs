fcd94568fdfea9911038c178caea5c83b06d655b
Huggyex86 <huggyex86@hotmail.it>
2015-09-07 11:48:19 +0200

    parola

--- Grammatica Beta/Form1.cs
+++ Grammatica Beta/Form1.cs
@@ -16,5 +16,10 @@ namespace Grammatica_Beta
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Grammatica Beta/Parola.cs
+++ Grammatica Beta/Parola.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Grammatica_Beta
 {
-    class Grammatica
+    class Parola
     {
-        
     }
 }
